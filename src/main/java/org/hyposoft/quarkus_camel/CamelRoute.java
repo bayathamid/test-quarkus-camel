f05fd63278616b34389cc74c92ec92ef37d8f4d6
bayathamid <bayathamid@gmail.com>
2023-11-25 09:50:53 +0330

    14020904

---
+++ src/main/java/org/hyposoft/quarkus_camel/CamelRoute.java
@@ -0,0 +1,21 @@
+package org.hyposoft.quarkus_camel;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+
+public class CamelRoute extends RouteBuilder{
+
+    @Override
+    public void configure() throws Exception {
+        from("timer://t1?period=1000")        
+        .setBody().simple("{\"id\":1,\"time\":\"${date-with-timezone:now:America/Los_Angeles:YYYY/MM/dd hh:mm:ss z}\"}")
+        .unmarshal().json(JsonLibrary.Jackson,Package.class)
+        .process(exchange -> {
+            Package package1 = exchange.getIn().getBody(Package.class);            
+            exchange.getMessage().setBody(package1.id);
+        })
+        .log("${body}");
+    }
+    
+
+}
---
+++ src/main/java/org/hyposoft/quarkus_camel/Package.java
@@ -0,0 +1,6 @@
+package org.hyposoft.quarkus_camel;
+
+public class Package {
+    public Integer id;
+    public String time;
+}
